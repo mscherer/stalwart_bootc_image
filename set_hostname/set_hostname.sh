1ab51eee7197cb0ea4292c46e39821f387b31364
Michael Scherer <misc@redhat.com>
2026-04-24 15:45:12 +0200

    Add a workaround, since ovh do not refresh the metadata

--- set_hostname/set_hostname.sh
+++ set_hostname/set_hostname.sh
@@ -3,7 +3,12 @@
 # get hostname from config
 # { echo -e "GET /1.0/meta-data/hostname HTTP/1.0\r\n\r" >&3; cat <&3 ; } 3<> /dev/tcp/169.254.169.254/80  | tail -1
 
-export HOSTNAME=$(curl http://169.254.169.254/1.0/meta-data/hostname)
+# since OVH doesn't let the hostname change after installation, I need a workaround
+if [ -f /etc/custom_hostname ]; then
+	export HOSTNAME=$(cat /etc/custom_hostname)
+else
+	export HOSTNAME=$(curl http://169.254.169.254/1.0/meta-data/hostname)
+fi;
 
 hostnamectl hostname $HOSTNAME
 
