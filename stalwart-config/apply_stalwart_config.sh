0561cc965e992ba5c09e03540a78d2b42159def6
Michael Scherer <misc@redhat.com>
2026-06-17 11:39:55 +0200

    Switch to using local interface

--- stalwart-config/apply_stalwart_config.sh
+++ stalwart-config/apply_stalwart_config.sh
@@ -3,8 +3,9 @@ cd /run/stalwart_config/
 
 /usr/local/bin/stalwart-recovery on
 
+set -a
 source /run/stalwart-recovery.env
-export STALWART_URL=http://127.0.0.1:8080/
+set +a
 
 for PLAN_FILE in *.json; do
 	/usr/local/bin/stalwart-cli apply --file ${PLAN_FILE}
